236db98365c26156ecd741c6d42ad84935c0a338
Liu <yuntliu@deloitte.com>
2018-05-31 22:31:55 -0600

    remove coe

--- coe_2.0/Draft/hpip/spend/SpendModels.scala
+++
@@ -1,5 +0,0 @@
-package com.anthem.hpip.spend
-
-case class VarianceSchema(tableName: String, partitionColumn: String, partitionValue: String,columnName: String,
-                          operationDescription: String, operation: String, operationValue: Double,
-                          percentageVariance: Double, threshold: Double,isThresholdCrossed:String,subjectArea:String)
