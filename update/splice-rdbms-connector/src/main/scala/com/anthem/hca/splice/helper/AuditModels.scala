1973f528d2c74c5ee7bb2226d93ab6be04ffa01c
Yuntao Liu <yuntliu@deloitte.com>
2019-01-17 17:07:59 -0700

    update

--- update/splice-rdbms-connector/src/main/scala/com/anthem/hca/splice/helper/AuditModels.scala
+++
@@ -1,6 +0,0 @@
-package com.anthem.hca.splice.helper
-
-import java.sql.Timestamp
-
-case class Audit(program: String, user_id: String, app_id: String, start_time: Timestamp, duration: String, status: String)
-
